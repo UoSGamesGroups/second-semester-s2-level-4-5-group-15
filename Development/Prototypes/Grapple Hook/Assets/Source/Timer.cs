bc498430775438eb183286e547b8cae856fa01e3
Benjamin Reynolds <benr420@gmail.com>
2017-02-08 21:58:17 +0000

    Create Timer class

---
+++ Development/Prototypes/Grapple Hook/Assets/Source/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Timer
+{
+
+    private float lastTick;
+    private readonly float interval;
+
+    public static float getTime()
+    {
+        return Time.realtimeSinceStartup;
+    }
+
+    public Timer(float interval)
+    {
+        this.interval = interval;
+        lastTick = Time.realtimeSinceStartup;
+    }
+
+    public bool hasElapsed()
+    {
+        return Time.realtimeSinceStartup - lastTick > interval;
+    }
+
+    public void reset()
+    {
+        lastTick = Time.realtimeSinceStartup;
+    }
+
+}
